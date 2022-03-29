b86eb4ed2e455c0659cdb472c21f1967582d3519
chemaguiniga2 <A01376669@itesm.mx>
2022-03-28 19:49:43 -0600

    final liks

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,4 +1,5 @@
-import { Spacer, Text, useTheme } from "@nextui-org/react"
+import { Link, Spacer, Text, useTheme } from "@nextui-org/react"
+import NextLink from 'next/link'
 import Image from "next/image"
 
 
@@ -14,16 +15,25 @@ export const Navbar = () => {
         padding: '0x 20px',
         backgroundColor: theme?.colors.gray900.value
     }}>
-        <Image
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
-            alt="icono de la app"
-            width={70}
-            height={70}
-        />
-        <Text color="white" h2>P</Text>
-        <Text color="white" h3>okémon</Text>
+        <NextLink href="/" passHref>
+          <Link>
+          
+            <Image
+                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+                alt="icono de la app"
+                width={70}
+                height={70}
+            />
+            <Text color="white" h2>P</Text>
+            <Text color="white" h3>okémon</Text>
+          </Link>
+        </NextLink>
         <Spacer css={{ flex: 1 }} />
-        <Text color="white">Favoritos</Text>
+        <NextLink href="/favoritos" passHref>
+          <Link>
+            <Text color="white">Favoritos</Text>
+          </Link>
+        </NextLink>
     </div>
   )
 }
